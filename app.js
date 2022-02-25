69a818c36849e3d53918e64d2dca846069008aff
dikaa7 <13518007@std.stei.itb.ac.id>
2022-02-24 18:28:41 -0800

    initial app

---
+++ app.js
@@ -0,0 +1,83 @@
+var vertexShaderText = 
+    `precision mediump float;
+
+    attribute vec2 vertPosition;
+    attribute vec3 vertColor;
+    varying vec3 fragColor;
+
+    void main() {
+        fragColor = vertColor;
+        gl_Position = vec4(vertPosition, 0.0, 1.0);
+    }`
+
+var fragmentShaderText = 
+    `precision mediump float;
+    
+    varying vec3 fragColor;
+    void main() {
+        gl_FragColor = vec4(fragColor, 1.0);
+    }`
+
+var createShader = function(type, source) {
+    var shader = gl.createShader(type)
+    gl.shaderSource(shader, source)
+    gl.compileShader(shader)
+
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+        console.error('Error compiling shader!', gl.getShaderInfoLog(shader))
+        gl.deleteShader(shader)
+        return
+    }
+    return shader
+}
+
+var createProgram = function(vertexShader, fragmentShader) {
+    var program = gl.createProgram()
+    gl.attachShader(program, vertexShader)
+    gl.attachShader(program, fragmentShader)
+    gl.linkProgram(program)
+    if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+        console.error('Error linking program!', gl.getProgramInfoLog(program))
+        gl.deleteProgram(program)
+        return
+    }
+    gl.validateProgram(program)
+    if (!gl.getProgramParameter(program, gl.VALIDATE_STATUS)) {
+        console.error('Error validating program!', gl.getProgramInfoLog(program))
+        gl.deleteProgram(program)
+        return
+    }
+    return program
+}
+
+var drawObject = function (program, vertices, method, n) {
+    var vertexBufferObject = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, vertexBufferObject);
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+
+	var positionAttribLocation = gl.getAttribLocation(program, 'vertPosition');
+    var colorAttribLocation = gl.getAttribLocation(program, 'vertColor');
+    gl.vertexAttribPointer(
+		positionAttribLocation, // Attribute location
+		2, // Number of elements per attribute
+		gl.FLOAT, // Type of elements
+		gl.FALSE,
+		5 * Float32Array.BYTES_PER_ELEMENT, // Size of an individual vertex
+		0 // Offset from the beginning of a single vertex to this attribute
+	);
+	gl.vertexAttribPointer(
+		colorAttribLocation, // Attribute location
+		3, // Number of elements per attribute
+		gl.FLOAT, // Type of elements
+		gl.FALSE,
+		5 * Float32Array.BYTES_PER_ELEMENT, // Size of an individual vertex
+		2 * Float32Array.BYTES_PER_ELEMENT // Offset from the beginning of a single vertex to this attribute
+	);
+
+	gl.enableVertexAttribArray(positionAttribLocation)
+	gl.enableVertexAttribArray(colorAttribLocation)
+
+	// Main render loop
+	gl.useProgram(program)
+    gl.drawArrays(method, 0, n)
+}

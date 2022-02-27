9b5998b47ed491b262411c83b81a0e6ad4df344a
andrc1613 <13519036@std.stei.itb.ac.id>
2022-02-27 15:16:53 +0700

    refactor shader code

--- app.js
+++ app.js
@@ -1,22 +1,6 @@
-var vertexShaderText = 
-    `precision mediump float;
+var vertexShaderText = document.getElementById("vertex-shader").text;
+var fragmentShaderText = document.getElementById("fragment-shader").text;
 
-    attribute vec2 vertPosition;
-    attribute vec3 vertColor;
-    varying vec3 fragColor;
-
-    void main() {
-        fragColor = vertColor;
-        gl_Position = vec4(vertPosition, 0.0, 1.0);
-    }`
-
-var fragmentShaderText = 
-    `precision mediump float;
-    
-    varying vec3 fragColor;
-    void main() {
-        gl_FragColor = vec4(fragColor, 1.0);
-    }`
 
 var createShader = function(type, source) {
     var shader = gl.createShader(type)
